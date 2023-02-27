6f811600b204152b7e4b04cc8659edebe4ace682
Ivelina Dimova <iv.dimova@gmail.com>
2023-02-27 13:06:54 +0100

    Add js for refreshing the block data with ajax

--- inc/ListUsersInfo.php
+++ inc/ListUsersInfo.php
@@ -21,12 +21,27 @@ class ListUsersInfo
     {
 
         add_action('wp_enqueue_scripts', [$this, 'ajaxUsersScript']);
-        add_action('wp_ajax_api_user_details', [$this, 'userDetails']);
-        add_action('wp_ajax_nopriv_api_user_details', [$this, 'userDetails']);
+        add_action('wp_ajax_api_user_details', [$this, 'usersInfo']);
+        add_action('wp_ajax_nopriv_api_user_details', [$this, 'usersInfo']);
     }
 
-    /**
+	 /**
      * Data for the user details ajax.
+     *
+     * @return void
+     */
+    public function usersInfo(): void
+    {
+		if ( ! isset( $_GET['nonce'] ) || ! wp_verify_nonce( $_GET['nonce'], 'api_user_details' ) ) {
+			wp_send_json_error( __( 'Unauthorized', 'list-users' ), 403 );
+		}
+		$users = new Data\ListUsersData();
+        $usersData = $users->allUsers();
+		wp_send_json_success($usersData);
+	}
+
+    /**
+     * Render user data.
      *
      * @return void
      */
@@ -35,13 +50,16 @@ class ListUsersInfo
 
         $users = new Data\ListUsersData();
         $usersData = $users->allUsers();
-
+		
         if (empty($usersData)) {
             return;
         } ?>        
-        <table id="user-details">
+        <table id="user-details"
+		action="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>"
+		data-nonce="<?php echo esc_attr( wp_create_nonce( 'api_user_details' ) ); ?>"
+		>
         <tbody>
-            <tr>
+            <th>
 				<?php if ( $idChecked !== true ) {  ?>
 					<th><?php echo esc_html__('User ID', 'list-users'); ?></th>
 				<?php } ?>
@@ -51,36 +69,40 @@ class ListUsersInfo
 				<?php if ( $emailChecked !== true ) { ?>
 					<th><?php echo esc_html__('Email', 'users'); ?></th>
 				<?php } ?>  
-            </tr>
+            </th>
 			<?php foreach ($usersData as $user) {
-            
-            $userName = isset($user['fname']) ? $user['fname'] : '';
-            $userLast = isset($user['lname']) ? $user['lname'] : '';
-            $userId = isset($user['id']) ? $user['id'] : '';
-            $userEmail = isset($user['email']) ? $user['email'] : '';
-        ?>
-                <tr>
-					<?php if ( $idChecked !== true ) { ?>
-						<td>
-							<?php echo esc_html($userId); ?>
-						</td>
-					<?php } ?>
-					<?php if ( $nameChecked !== true ) { ?>
-						<td>
-							<?php echo esc_html($userName) . ' ' . esc_html($userLast); ?>
-						</td>
-						<?php } ?>
-						<?php if ( $emailChecked !== true ) { ?>
-							<td>
-								<?php echo esc_html($userEmail); ?>
-							</td>
-						<?php } ?>
-                </tr>
-				<?php
-        } ?>
+				
+				$userName = isset($user['fname']) ? $user['fname'] : '';
+				$userLast = isset($user['lname']) ? $user['lname'] : '';
+				$userId = isset($user['id']) ? $user['id'] : '';
+				$userEmail = isset($user['email']) ? $user['email'] : '';
+				?>
+            <tr class="user-data">
+				<?php if ( $idChecked !== true ) { ?>
+					<td class="user_id">
+						<?php echo esc_html($userId); ?>
+					</td>
+				<?php } ?>
+				<?php if ( $nameChecked !== true ) { ?>
+					<td class="user_name">
+						<?php echo esc_html($userName) . ' ' . esc_html($userLast); ?>
+					</td>
+				<?php } ?>
+				<?php if ( $emailChecked !== true ) { ?>
+					<td class="user_email">
+						<?php echo esc_html($userEmail); ?>
+					</td>
+				<?php } ?>
+            </tr>
+		<?php
+        } 
+		?>
             </tbody>
+			<button id="users-udpate" type='submit'>
+				<?php esc_html_e( 'Update results', 'list-users' ); ?>
+			</button>
         </table>
-<?php
+	<?php
     }
 
     /**
--- inc/js/custom.js
+++ inc/js/custom.js
@@ -1,14 +1,46 @@
-jQuery(function ($) {
-
-	jQuery.ajax({
-        type: 'POST',
-        url :  ajax_object.url,
-        data: { action: 'api_user_details', nonce: ajax_object.nonce  },
-        success: function (data) {
-        jQuery("#users-table").html('');
-                jQuery("#users-table").append(data);
-        },
-        error: function ( errorThrown) {
-            alert(errorThrown); }
-        });
+const table = document.getElementById( 'user-details' );
+const button = document.getElementById('users-udpate');
+button.addEventListener('click', (event) => {
+	update_table(table);
 });
+	
+function update_table(table) {
+	actionUrl = table.getAttribute('action');
+	nonce = table.getAttribute('data-nonce');
+
+	const data = {
+		'action': 'api_user_details',
+		'nonce': nonce,
+	}
+
+	const url = new URL( actionUrl );
+	url.search = new URLSearchParams( data ).toString();
+	fetch( url, {
+		headers: {
+			'Content-Type': 'application/x-www-form-urlencoded',
+			'Cache-Control': 'no-cache',
+		},
+	} )
+		.then( response => response.json() )
+		.then( response => {
+			const tableBody = table.querySelector('tbody');
+			let newTableBody = document.createElement('tbody');
+			Object.values(response.data).forEach(val => {
+				let newRow = document.createElement('tr');
+				let newName = document.createElement('td');
+				let newId = document.createElement('td');
+				let newEmail = document.createElement('td');
+				newName.className = "user_name";
+				newName.innerHTML = val.fname;
+				newId.className = "user_id";
+				newId.innerHTML = val.id;
+				newEmail.className = "user_email";
+				newEmail.innerHTML = val.email;
+				newRow.appendChild(newId);
+				newRow.appendChild(newName);
+				newRow.appendChild(newEmail);
+				newTableBody.append(newRow);
+			  });	
+			  tableBody.replaceWith( newTableBody );
+		});
+}
